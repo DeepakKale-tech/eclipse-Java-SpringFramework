b5db3b20ddad1b9a614d9505381ca4a8646215dc
Deepak Kale <kaledeepak4646@gmail.com>
2026-02-20 08:58:26 +0530

    annotation commit

---
+++ SpringAnnotationDemo/src/com/config/annotations/App.java
@@ -0,0 +1,22 @@
+package com.config.annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/config/annotations/spring.xml");
+		
+		Instructor instructor = (Instructor)context.getBean("inst");
+		System.out.println(instructor.hashCode());
+		System.out.println(instructor);
+		
+		Instructor instructor2 = (Instructor)context.getBean("inst");
+		System.out.println(instructor2.hashCode());
+		System.out.println(instructor2);
+	}
+
+}
---
+++ SpringAnnotationDemo/src/com/config/annotations/Instructor.java
@@ -0,0 +1,37 @@
+package com.config.annotations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("inst")
+@Scope("prototype")
+public class Instructor {
+
+	@Value("101")
+	private int id;
+	@Value("Deepak")
+	private String name;
+	@Value("#{topics}")
+	private List<String> topics;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + "]";
+	}
+	
+	
+}
